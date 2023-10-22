1a3adc9675fb23adfcea64e3110a65f8df1846ce
icebee03 <muffin2003.mm@gmail.com>
2023-10-23 01:48:35 +0200

    messing with matplotlib and plotting of 1 predefined function

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def f(x):
+    return 2 * x ** 2 + 3 * x + 5
+
+
 def main():
-    print("Hello World!")
+    x, y = [], []
+    for i in range(-10, 10, 1):
+        x.append(i)
+        y.append(f(i))
+
+    plt.plot(x, y, color="r")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title(r"Plot of $2x^2 + 3x + 5$")       #TeX format inside dollar signs
+    plt.show()
 
 
 if __name__ == "__main__":
